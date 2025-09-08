87a2d092a43878a88b0eceb5c0d006fe313532bd
johney <computerjohney@gmail.com>
2025-09-08 14:27:05 +1200

    assets directories and texture packer in new utils module with main()

--- utils/src/main/java/com/computerjohney/utils/TexturePackerTool.java
+++ utils/src/main/java/com/computerjohney/utils/TexturePackerTool.java
@@ -1,9 +1,16 @@
 package com.computerjohney.utils;
 
+import com.badlogic.gdx.tools.texturepacker.TexturePacker;
+
 public class TexturePackerTool {
 
     public static void main(String[] args){
+        String inputDir = "assets_raw/objects";
+        String outputDir = "assets/graphics";
+        // filename of texture atlas
+        String packFileName = "objects";
 
+        TexturePacker.process(inputDir, outputDir, packFileName);
     }
 
 }
